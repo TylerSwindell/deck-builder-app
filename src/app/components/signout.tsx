6e7824292fa02effb764c4e451f3df80b1ab359e
Levon Ritter <lritter1@ottertune.com>
2023-06-20 01:08:04 -0400

    got signout to work

--- src/app/components/signout.tsx
+++ src/app/components/signout.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+'use client';
 
-const Signout = () => {
-  return <div>signout</div>;
-};
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { useRouter } from 'next/navigation';
 
-export default Signout;
+export default function Signout() {
+  const router = useRouter();
+  const supabase = createClientComponentClient();
+
+  const handleSignOut = async () => {
+    await supabase.auth.signOut();
+    router.refresh();
+  };
+
+  return (
+    <>
+      <button onClick={handleSignOut}>Sign out</button>
+    </>
+  );
+}
