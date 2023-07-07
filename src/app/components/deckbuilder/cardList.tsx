d5121d151fe6f8736edb61e25457a7997eac508f
Levon Ritter <lritter1@ottertune.com>
2023-07-07 09:15:16 -0400

    revised card list to include quantity

--- src/app/components/deckbuilder/cardList.tsx
+++ src/app/components/deckbuilder/cardList.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+'use client';
+
+import { useState } from 'react';
 
 interface CardProps {
   name: string;
@@ -11,13 +13,45 @@ interface Props {
 }
 
 const CardList: React.FC<Props> = ({ items, callback }) => {
+  const [selectedNumbers, setSelectedNumbers] = useState<{
+    [key: number]: number;
+  }>({});
+
+  const handleNumberChange = (
+    multiverseid: number,
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const selectedNumber = parseInt(event.target.value);
+    setSelectedNumbers((prevSelectedNumbers) => ({
+      ...prevSelectedNumbers,
+      [multiverseid]: selectedNumber,
+    }));
+  };
   return (
-    <div className="bg-white rounded p-4 mt-1 ">
-      <div className="grid grid-cols-3 gap-4">
+    <div className="bg-white rounded p-4 mt-1">
+      <div className="grid grid-cols-1 gap-4">
         {items.map((item) => (
-          <div key={item.multiverseid}>
-            <div className="flex items-center">
+          <div key={item.multiverseid} className="flex items-center">
+            <div>
               <span>{item.name}</span>
+            </div>
+            <div className="ml-auto flex items-center">
+              <select
+                className="border border-gray-300 px-2 py-1 w-50"
+                value={selectedNumbers[item.multiverseid] || ''}
+                onChange={(event) =>
+                  handleNumberChange(item.multiverseid, event)
+                }
+              >
+                {Array.from(
+                  { length: 100 },
+                  (_, index) => index + 1
+                ).map((number) => (
+                  <option key={number} value={number}>
+                    {number}
+                  </option>
+                ))}
+              </select>
               <button
                 className="ml-2 text-red-500"
                 onClick={() => callback(item.multiverseid)}
